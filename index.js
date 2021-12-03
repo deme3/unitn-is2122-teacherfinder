47e7dee6527702cf96603139ac27d1bf1e6a1564
Demetrio Battaglia <deme3.iot@gmail.com>
2021-12-03 19:26:38 +0100

    Cambiato da Vue.js remoto a Vue.js modulo ES6 (NPM)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import Vue from '../node_modules/vue/dist/vue.esm.browser.js';
+
 Vue.component('search-result', {
     props: {"title": String, "price": Number, "grading": Number},
     computed: {
