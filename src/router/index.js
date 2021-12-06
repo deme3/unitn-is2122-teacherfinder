b85c51c4e3c619e72a30fd3ad563f4c67fc0b2cf
riklus <lussana.riccardo@gmail.com>
2021-12-06 01:09:31 +0100

    Aggiunta view iscrizioni

--- src/router/index.js
+++ src/router/index.js
@@ -9,13 +9,13 @@ const routes = [
     props: true,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/iscrizioni",
+    name: "Iscrizioni",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Iscrizioni.vue"),
   },
 ];
 
