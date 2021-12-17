cbdd32cb4cf68df9fbefee1c47b954c63261c343
Demetrio Battaglia <deme3.iot@gmail.com>
2021-12-17 16:03:50 +0100

    Ibrido hostname per contattare API dev env e prod env

--- src/main.js
+++ src/main.js
@@ -2,4 +2,24 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.config.globalProperties.$isDevEnv = () => {
+  // Vue CLI Service definisce process.env quando avvia il development server
+  // Se trovo process.env e process.env.NODE_ENV è assegnato a development
+  return (
+    typeof process !== "undefined" &&
+    typeof process.env !== "undefined" &&
+    typeof process.env.NODE_ENV !== "undefined" &&
+    process.env.NODE_ENV === "development");
+};
+
+app.config.globalProperties.$apiBaseURL = () => {
+  let port = app.config.globalProperties.$isDevEnv()
+    ? 8080
+    : window.location.port;
+  return `http://${window.location.hostname}:${port}/api`
+};
+
+app.use(router);
+app.mount("#app");
