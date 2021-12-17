4bfd5450250c7cac0fe2e726282c554672850342
riklus <lussana.riccardo@gmail.com>
2021-12-17 23:24:07 +0100

    Provide di apiBaseURL

--- src/main.js
+++ src/main.js
@@ -4,23 +4,14 @@ import router from "./router";
 
 const app = createApp(App);
 
-app.config.globalProperties.$isDevEnv = () => {
-  // Vue CLI Service definisce process.env quando avvia il development server
-  // Se trovo process.env e process.env.NODE_ENV è assegnato a development
-  return (
-    typeof process !== "undefined" &&
-    typeof process.env !== "undefined" &&
-    typeof process.env.NODE_ENV !== "undefined" &&
-    process.env.NODE_ENV === "development"
-  );
-};
-
-app.config.globalProperties.$apiBaseURL = () => {
-  let port = app.config.globalProperties.$isDevEnv()
-    ? 8080
-    : window.location.port;
-  return `http://${window.location.hostname}:${port}/api`;
-};
+app.provide(
+  "apiBaseURL",
+  (() => {
+    if (process?.env?.NODE_ENV === "development")
+      return `http://${window.location.hostname}:8080`;
+    else return "";
+  })()
+);
 
 app.use(router);
 app.mount("#app");
