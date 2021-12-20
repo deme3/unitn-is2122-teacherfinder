dd6a5e86b980c02473362feefe19c84a1fd32660
Demetrio Battaglia <deme3.iot@gmail.com>
2021-12-20 11:32:17 +0100

    Generalizzazione componente stelline

---
+++ src/components/RatingStars.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :class="ratingClass">
+    <div class="star" v-for="n in 5" :key="n" />
+  </div>
+</template>
+
+<style scoped>
+.star-rating {
+  margin-top: 10px;
+}
+
+.star {
+  width: 16px;
+  height: 16px;
+  background-image: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="gold" fill="gold" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" fill="gold"></path></svg>');
+  background-size: cover;
+  display: inline-block;
+  margin: 0 4px;
+}
+
+.star:first-child {
+  margin-left: 0;
+}
+
+.star-rating.star-0 .star {
+  display: none;
+}
+
+.star-rating.star-0:after {
+  content: "Nessuna valutazione";
+  vertical-align: top;
+  line-height: 1.1em;
+  color: goldenrod;
+}
+
+.star-rating.star-1 .star:not(:first-child),
+.star-rating.star-2 .star:not(:first-child, :nth-child(2)),
+.star-rating.star-3 .star:not(:first-child, :nth-child(2), :nth-child(3)),
+.star-rating.star-4 .star:nth-child(4),
+.star-rating .star-4 .star:last-child {
+  opacity: 0.2;
+}
+</style>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  rating: Number,
+});
+
+const ratingClass = computed(() => {
+  return "star-rating star-" + props.rating;
+});
+</script>
--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="sr-info tf-box" @click="onClick">
     <div class="sr-title">{{ props.title }}</div>
-    <div class="sr-price">Prezzo: {{ prettyPrice }}€/ora</div>
-    <div :class="ratingClass">
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-    </div>
+    <div class="sr-price">Prezzo: {{ props.price.toFixed(2) }}€/ora</div>
+    <RatingStars :rating="props.rating" />
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
 import { useRouter } from "vue-router";
+import RatingStars from "@/components/RatingStars.vue";
 
 const router = useRouter();
 
@@ -26,13 +20,6 @@ const props = defineProps({
 });
 
 const emit = defineEmits(["offer-click"]);
-
-const ratingClass = computed(() => {
-  return "sr-grading star-" + props.rating;
-});
-const prettyPrice = computed(() => {
-  return props.price.toFixed(2);
-});
 
 const onClick = () => {
   router.push({ name: "Annuncio", params: { id: props.id } });
@@ -54,41 +41,5 @@ const onClick = () => {
 .sr-info .sr-title {
   font-size: 16pt;
   margin-bottom: 8px;
-}
-
-.sr-grading {
-  margin-top: 10px;
-}
-
-.star {
-  width: 16px;
-  height: 16px;
-  background-image: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="gold" fill="gold" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" fill="gold"></path></svg>');
-  background-size: cover;
-  display: inline-block;
-  margin: 0 4px;
-}
-
-.star:first-child {
-  margin-left: 0;
-}
-
-.sr-grading.star-0 .star {
-  display: none;
-}
-
-.sr-grading.star-0:after {
-  content: "Nessuna valutazione";
-  vertical-align: top;
-  line-height: 1.1em;
-  color: goldenrod;
-}
-
-.sr-grading.star-1 .star:not(:first-child),
-.sr-grading.star-2 .star:not(:first-child, :nth-child(2)),
-.sr-grading.star-3 .star:not(:first-child, :nth-child(2), :nth-child(3)),
-.sr-grading.star-4 .star:nth-child(4),
-.sr-grading .star-4 .star:last-child {
-  opacity: 0.2;
 }
 </style>
