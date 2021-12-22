876bbdfa34347c2502c1c49ee5303cb29c3259a9
riklus <lussana.riccardo@gmail.com>
2021-12-22 23:22:58 +0100

    Iscrizioni non disponibili. Aggiunto avviso.

--- src/views/Iscrizioni.vue
+++ src/views/Iscrizioni.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="iscrizioni">
     <h1>Le mie iscrizioni</h1>
+    <SmallBox text="Non disponibile" />
   </div>
 </template>
 
 <script setup>
+import SmallBox from "@/components/SmallBox.vue";
+
 document.title = "TeacherFinder – Le mie iscrizioni";
 </script>
